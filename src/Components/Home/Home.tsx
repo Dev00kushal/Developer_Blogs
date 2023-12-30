b37908374886cbf3f2c5f12ce747e82a54734c4b
Devkushal <devkushal00@gmail.com>
2023-12-30 01:13:47 -0500

    Refactor timeline rendering in Home component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,47 @@
 import { useRef } from "react";
 import Button from "../Button/Button";
 import useHomeGsap from "../../Utils/Home.gsap";
-import {TimelineContent} from "../../Constants/Home.constants";
+import { TimelineContent } from "../../Constants/Home.constants";
+
+const TimelineItem = ({
+  title,
+  description,
+  isStart,
+}: {
+  title: string;
+  description: string;
+  isStart: boolean;
+}) => (
+  <li>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className={`timeline-${isStart ? "start" : "end"}`}>
+      <div className="text-lg font-black underline">{title}</div>
+      {description}
+    </div>
+    <hr />
+  </li>
+);
+
 const Home = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
   useHomeGsap();
+
+  const backgroundImageUrl = "../public/mock.gif";
+  const writingGifUrl = "../public/writing.gif";
+
   return (
     <div data-scroll-container ref={scrollRef}>
       <div className="mockup-browser border bg-base-300 scale-75 mb-10">
@@ -14,7 +51,7 @@ const Home = () => {
         <div
           className="hero min-h-screen"
           style={{
-            backgroundImage: "url(../public/mock.gif)",
+            backgroundImage: `url(${backgroundImageUrl})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
@@ -50,151 +87,40 @@ const Home = () => {
       <div className="scale-90 mb-20 ">
         <h1 className="text-5xl white font-mono ">Why are we here ?</h1>
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-          <li>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-start md:text-end ">
-              <div className="text-lg font-black underline">
-              {TimelineContent[0].title}
-              </div>
-              {TimelineContent[0].description}
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end ">
-              <div className="text-lg font-black underline">
-              {TimelineContent[1].title}
-              </div>
-              {TimelineContent[1].description}
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-start md:text-end ">
-              <div className="text-lg font-black underline">
-              {TimelineContent[2].title}
-              </div>
-              {TimelineContent[2].description}
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end">
-              <div className="text-lg font-black underline">{TimelineContent[3].title}</div>
-              {TimelineContent[3].description}
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-start md:text-end ">
-              <div className="text-lg font-black underline">
-              {TimelineContent[4].title}
-              </div>
-              {TimelineContent[4].description}
-            </div>
-            <hr />
-          </li>
+          {TimelineContent.map((content, index) => (
+            <TimelineItem
+              key={index}
+              title={content.title}
+              description={content.description}
+              isStart={index % 2 === 0}
+            />
+          ))}
         </ul>
       </div>
       <div className="flex items-center">
         <div className="mockup-phone border-primary ml-10 scale-100">
           <div className="camera"></div>
-          <div className="display">
-            <div
-              className="artboard artboard-demo phone-1"
-              style={{
-                backgroundImage: "url(../public/writing.gif)",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                height: "500px",
-                width: "250px",
-                borderRadius: "8px",
-                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            ></div>
-          </div>
+          <div
+            className="artboard artboard-demo phone-1"
+            style={{
+              backgroundImage: `url(${writingGifUrl})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              height: "500px",
+              width: "250px",
+              borderRadius: "8px",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          ></div>
         </div>
         <div className="ml-5">
           <h1 className="text-5xl font-bold">
-            Write your stories,Gossips,experiences!
+            Write your stories, Gossips, experiences!
           </h1>
           <p className="py-6 mr-20">
             Its a platform where narratives unfold, secrets are shared, and
